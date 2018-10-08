d5dc95799728c862735554e8f6cfb275138d7835
talexcrowell <talexcrowell@gmail.com>
2018-10-08 12:18:49 -0700

    added detailed endpoints

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ app.get('/api/notes', (req, res) => {
   res.json(data);
 });
 
+app.get('/api/notes/:id', (req, res) =>{
+  const id = parseInt(req.params.id, 10);
+  res.json(data.find(item => item.id === id));
+});
+
 app.listen(8080, () => {
   console.info(`Server listening on 8080`);
 }).on('error', err => {
