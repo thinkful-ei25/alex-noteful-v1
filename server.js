79f7d8fc83b5be4a9f29849f7df18ec23fe9753b
talexcrowell <talexcrowell@gmail.com>
2018-10-10 11:28:40 -0700

    added router folder with notes.router and rewired server.js

--- server.js
+++ server.js
@@ -1,69 +1,20 @@
 'use strict';
 
-// Load array of notes
-const data = require('./db/notes');
-const simDB = require('./db/simDB');
-const notes = simDB.initialize(data);
-
-const { PORT } = require('./middleware/config');
-
 console.log('Hello Noteful!');
 
 const express = require('express');
 const morgan = require('morgan');
-const app = express();
 
+const { PORT } = require('./middleware/config');
+const notesRouter = require('./router/notes.router');
+
+const app = express();
 
 app.use(morgan('dev'));
 app.use(express.static('public'));
 app.use(express.json());
 
-app.get('/api/notes/',  (req, res, next) => {
-  const { searchTerm } = req.query;
-
-  notes.filter(searchTerm, (err, list) => {
-    if (err) {
-      return next(err); // goes to error handler
-    }
-    res.json(list); // responds with filtered array
-  });
-});
-
-
-app.get('/api/notes/:id', (req, res) =>{
-  const id = parseInt(req.params.id, 10);
-
-  notes.find(id, (err, list) => {
-    if (err) {
-      return next(err);
-    }
-    res.json(list);
-  });
-});
-
-app.put('/api/notes/:id', (req, res, next) => {
-  const id = req.params.id;
-
-  const updateObj={};
-  const updateFields = ['title', 'content'];
-
-  updateFields.forEach(field => {
-    if (field in req.body) {
-      updateObj[field] = req.body[field];
-    }
-  });
-
-  notes.update(id, updateObj, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.json(item);
-    } else {
-      next();
-    }
-  });
-});
+app.use('/api/notes', notesRouter);
 
 
 app.use(function (req, res, next){
